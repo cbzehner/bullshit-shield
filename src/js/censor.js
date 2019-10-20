6c581e5ebec6e4e9b871e2d5c72280d10ef60865
Chris Zehner <cbzehner@gmail.com>
2019-10-20 07:29:30 -0500

    feat: replace homegrown DOM Walker with findAndReplaceDOMText

--- src/js/censor.js
+++ src/js/censor.js
@@ -1,6 +1,8 @@
 /**
  * Walk the webpage, censoring terms
  */
+import findAndReplaceDOMText from "findandreplacedomtext"
+import bullshitTerms from "./terms"
 
 // const censor = () => {
 //   chrome.runtime.sendMessage({ message: "censoredTerms" }, response => {
@@ -10,40 +12,16 @@
 //   })
 // }
 
-const censorBullshit = bullshitTerms => {
-  const root = document.body
-  const walker = document.createTreeWalker(
-    root,
-    NodeFilter.SHOW_ALL,
-    null,
-    false
-  )
-
-  const terms = new Set(["Chrome", "extension"])
+const censorBullshit = () => {
+  const bullshitRe = new RegExp(`\\b(${bullshitTerms.join("|")})\\b`, "gi")
   const redactionClass = "blurred" // Options: blurred invisible-ink redacted
 
-  // https://stackoverflow.com/questions/15553280/replace-a-textnode-with-html-text-in-javascript
-  // https://devdocs.io/dom/childnode/replacewith
-  while (walker.nextNode()) {
-    const current = walker.currentNode
-    const isCandidateNode =
-      current.childElementCount === 0 || current.tagName === "P"
-
-    if (isCandidateNode && current.textContent) {
-      terms.forEach(term => {
-        if (current.textContent.includes(term)) {
-          debugger
-          // TODO: Avoid splitting the terms if they're part of a hyperlink
-          const contents = current.innerHTML.split(term)
-          current.innerHTML = contents.join(
-            `<span class="${redactionClass}">${term}</span>`
-          )
-        }
-      })
-      walker.nextNode() // Step over the current node
-      walker.nextNode() // Step over the newly inserted span
-    }
-  }
+  findAndReplaceDOMText(document.body, {
+    find: bullshitRe,
+    wrap: "span",
+    wrapClass: redactionClass,
+    preset: "prose",
+  })
 }
 
 censorBullshit()
